3f63f8972d10503b727897bd272c7969508aafa8
Debargha Nandi <debarghanandi.india@gmail.com>
2024-02-02 23:57:40 +0530

    project 36

---
+++ HTML,CSS,Js/project 36 bargand wp clone/script.js
@@ -0,0 +1,8 @@
+// for highlited color change
+// const colors = ["#c1b001", "#a8140e", "#4315aa", "#359d09", "#8f4762"];
+
+// window.addEventListener("mousedown", (e) => {
+//   const color = colors.shift();
+//   document.documentElement.style.setProperty("--highlight-color", color);
+//   colors.push(color);
+// });
