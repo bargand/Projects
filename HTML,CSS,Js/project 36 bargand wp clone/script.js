e00e81738c99787f08b59e8f5dd6d1c89c01f3ab
Debargha Nandi <debarghanandi.india@gmail.com>
2024-02-04 01:55:03 +0530

    project36

--- HTML,CSS,Js/project 36 bargand wp clone/script.js
+++ HTML,CSS,Js/project 36 bargand wp clone/script.js
@@ -1,9 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// active page
