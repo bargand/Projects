fa12a74f405bf38155fd1f3f75a65262f5053922
Debargha Nandi <debarghanandi.india@gmail.com>
2024-06-20 22:26:46 +0530

    React

--- React/10. Traffic Light/src/App.jsx
+++ React/10. Traffic Light/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useState} from 'react'
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [color, setColor] = useState(true)
+  const divStyle = {
+    background: color? "green": "red"
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main">
+      <div className="firstDiv" style={divStyle}></div>
+      <div className="secondDiv">
+        <div className="secondDivA"></div>
+        <div className="secondDivB"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="thirdDiv"></div>
+    </div>
     </>
   )
 }
