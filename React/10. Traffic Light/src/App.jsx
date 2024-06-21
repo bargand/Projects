227861462c9d87f6b8d81ca4a328879bd37e9a19
Debargha Nandi <debarghanandi.india@gmail.com>
2024-06-21 12:07:08 +0530

    Traffic Light

--- React/10. Traffic Light/src/App.jsx
+++ React/10. Traffic Light/src/App.jsx
@@ -1,24 +1,56 @@
-import React,{useState} from 'react'
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
-  const [color, setColor] = useState(true)
-  const divStyle = {
-    background: color? "green": "red"
-  }
+  const [color, setColor] = useState("green");
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setColor((prevColor) => (prevColor === "green" ? "red" : "green"));
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (color === "green") {
+      document.querySelector(".firstDiv1").style.backgroundColor = "red"
+      document.querySelector(".thirdDiv1").style.backgroundColor = "red"
+    }
+    else{
+      document.querySelector(".firstDiv1").style.backgroundColor = "green"
+      document.querySelector(".thirdDiv1").style.backgroundColor = "green"
+    }
+  })
+  
 
   return (
     <>
-    <div className="main">
-      <div className="firstDiv" style={divStyle}></div>
-      <div className="secondDiv">
-        <div className="secondDivA"></div>
-        <div className="secondDivB"></div>
+      <div className="main">
+        <div className="firstDiv">
+          <div className="firstDiv1"></div>
+          <div className="firstDiv2"></div>
+          <div className="firstDiv3"></div>
+        </div>
+        <div className="secondDiv">
+          <div className="secondDivA">
+          <div className="secondDivA1" style={{ backgroundColor: color }}></div>
+          <div className="secondDivA2"></div>
+          <div className="secondDivA3"></div>
+          </div>
+          <div className="secondDivB" >
+          <div className="secondDivB1" style={{ backgroundColor: color }}></div>
+          <div className="secondDivB2"></div>
+          <div className="secondDivB3"></div>
+          </div>
+        </div>
+        <div className="thirdDiv">
+          <div className="thirdDiv1"></div>
+          <div className="thirdDiv2"></div>
+          <div className="thirdDiv3"></div>
+        </div>
       </div>
-      <div className="thirdDiv"></div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
